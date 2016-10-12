4e3472c80be0412d042d680dc7813bfbadfd6b1e
Lijun Chen <lic062@eng.ucsd.edu>
2016-10-12 14:54:47 -0700

    restart

--- app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/PictureUtils.java
@@ -1,8 +1,11 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by ljchen on 10/6/16.
  */
 
 public class PictureUtils {
+//    public static Bitmap getScaledBitMap(String path)
 }
